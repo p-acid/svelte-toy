6d891828ad8d9fbcfc4746fa06ffd6318ec7bdf7
p-acid <qkrtkstjd01@gmail.com>
2022-03-09 21:21:16 +0900

    Modify: global css

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,18 +1,35 @@
 <script>
     import { link } from 'svelte-spa-router'
     import active from 'svelte-spa-router/active'
+
+    const headerItems = [
+        {
+            url: '/',
+            name: 'Home'
+        },
+        {
+            url: '/about',
+            name: 'About'
+        }
+    ]
 </script>
 
 <header>
-    <a href='/' use:link use:active>
-        Home
-    </a>
-    <a href='/about' use:link use:active>
-        About
-    </a>
+    {#each headerItems as item}
+        <a href={item.url} use:link use:active>
+            {item.name}
+        </a>
+    {/each}
 </header>
 
 <style>
+    header {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+        width: 100%;
+    }
+
     :global(header a.active) {
       font-weight: bold;
       text-decoration: underline;
